b7b8863b63a7fe394a5e1e828fbb2b49f7e2e716
FleurBongaerts <f.l.p.bongaerts@students.uu.nl>
2017-06-08 17:02:04 +0200

    updated

--- letters.py
+++ letters.py
@@ -10,8 +10,9 @@ with open('age_big.json', 'r') as file:
     actordata = json.load(file)
 i=0
 for actor in actordata:
-    actor['1stletter']=actor['rdf-schema#label'][0]
-    
-
+   actor['1stletter']=actor['rdf-schema#label'][0]
+   if str.isalpha(actor['rdf-schema#label'][0]) == False:
+       i+=1
+       actor['rdf-schema#label']=None
 with open('letters_big.json', 'w') as file:                        
     json.dump(actordata, file, indent=4, ensure_ascii=True)
