28d6315a7004e90dbbf7de4ffda7b4866495ce17
FleurBongaerts <f.l.p.bongaerts@students.uu.nl>
2017-06-08 14:59:30 +0200

    letter key add

---
+++ letters.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 14:49:31 2017
+
+@author: Algemeen
+"""
+
+import json
+with open('age_big.json', 'r') as file:
+    actordata = json.load(file)
+i=0
+for actor in actordata:
+    actor['1stletter']=actor['rdf-schema#label'][0]
+    
+
+with open('letters_big.json', 'w') as file:                        
+    json.dump(actordata, file, indent=4, ensure_ascii=True)
