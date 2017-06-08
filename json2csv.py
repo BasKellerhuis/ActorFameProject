a9ef2c27988366f3ec4c7606f5438bdb04ff7322
BasKellerhuis <bas@kellerhuis.com>
2017-06-08 16:36:31 +0200

    csv2json convert that finds more actors than the previous version.

--- json2csv.py
+++ json2csv.py
@@ -7,11 +7,11 @@ Created on Wed Jun  7 16:25:52 2017
 
 import csv, json
 
-with open('age_big.json', 'r') as file:
+with open('letters_big.json', 'r') as file:
     actordata = json.load(file)
 
 with open('actorfame.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile,quoting=csv.QUOTE_NONNUMERIC)
-    spamwriter.writerow(['Name', 'Age', 'CountryOfOrigin', 'FirstActiveYear', 'PageCreationYear', 'FameScore'])
+    spamwriter.writerow(['Name', 'Age', 'CountryOfOrigin', 'FirstActiveYear', 'PageCreationYear', 'FameScore', 'FirstLetter'])
     for actor in actordata:
-        spamwriter.writerow([actor['rdf-schema#label'].encode('utf-8'), actor['age'], actor['stateOfOrigin_label'], actor['activeYearsStartYear'], actor['creationYear'], actor['fame']])
+        spamwriter.writerow([actor['rdf-schema#label'].encode('utf-8'), actor['age'], actor['stateOfOrigin_label'], actor['activeYearsStartYear'], actor['creationYear'], actor['fame'], actor['1stletter'].encode('utf-8')])
---
+++ rawcsv2json_version2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# (c) 2016 David A. van Leeuwen
+
+## This file converts a "raw" tye of csv file from the PoW database into a json.
+
+## Specifically,
+## - we use a short label (first line in the general CSV header)
+## - "NULL" entries are simply left out
+## - numbers are interpreted as numbers, not strings
+
+import argparse, json, logging, csv, re, sys, codecs, os
+
+
+floatre = re.compile("^\d+\.\d+$")
+intre = re.compile("^\d+$")
+actors = []
+i = 0
+
+def read_header(file="h.txt"):
+    header=[]
+    for line in open(file):
+        header.append(line.strip())
+    logging.info("%d lines in header", len(header))
+    return header
+
+def process_csv(file, header):
+    out=[]
+    stdin = file == "-"
+    fd = sys.stdin if stdin else codecs.open(file, 'r', 'UTF-8')
+    reader = csv.reader(fd)
+    for nr, row in enumerate(reader):
+        logging.debug("%d fields in line %d", len(row), nr)
+        d = dict()
+        out.append(d)
+        for i, field in enumerate(row):
+            if field != "NULL":
+                if floatre.match(field):
+                    d[header[i]] = float(field)
+                elif intre.match(field):
+                    d[header[i]] = int(field)
+                else:
+                    d[header[i]] = field
+    if not stdin:
+        fd.close()
+    return out
+
+
+header = read_header("h.txt")
+for file in os.listdir("D:/Documents/UCU/Courses/Data Analysis/Assignment 6 - Wikipedia/years/"):
+    out = process_csv("years/" + file, header)
+    #print(json.dumps(out, indent=4, ensure_ascii=True))
+    print (file)
+    for entry in out:
+        if "22-rdf-syntax-ns#type_label" in entry:
+        
+            if entry["22-rdf-syntax-ns#type_label"] == 'Actor' or entry["22-rdf-syntax-ns#type_label"] == 'Actress':
+                actors.append(entry)
+        
+            else: 
+                split_occupation = entry["22-rdf-syntax-ns#type_label"][1:-1].split('|')
+                for i in range(len(split_occupation)):
+                    if split_occupation[i] == 'Actor' or split_occupation[i] == 'Actress' or split_occupation[i] == 'Actor109765278':
+                        actors.append(entry)
+                            
+with open('actors_big.json', 'w') as file:                        
+    json.dump(actors, file, indent=4, ensure_ascii=True)
