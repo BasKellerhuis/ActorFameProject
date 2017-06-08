19c6eb41301c121fd12efa610f7a7bb69882dee2
FleurBongaerts <f.l.p.bongaerts@students.uu.nl>
2017-06-08 17:12:59 +0200

    Updated AGAIN

--- letters.py
+++ letters.py
@@ -4,15 +4,16 @@ Created on Thu Jun  8 14:49:31 2017
 
 @author: Algemeen
 """
-
+import string
 import json
 with open('age_big.json', 'r') as file:
     actordata = json.load(file)
 i=0
 for actor in actordata:
    actor['1stletter']=actor['rdf-schema#label'][0]
-   if str.isalpha(actor['rdf-schema#label'][0]) == False:
+   if not (actor['rdf-schema#label'][0]) in list(string.ascii_uppercase):
        i+=1
-       actor['rdf-schema#label']=None
+       actor['1stletter'] = None
+            
 with open('letters_big.json', 'w') as file:                        
     json.dump(actordata, file, indent=4, ensure_ascii=True)
